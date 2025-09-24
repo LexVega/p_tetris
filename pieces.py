769fdd7635cb763125bcc722640201d7cfbc0a07
LexVega <smallgrown@gmail.com>
2025-09-24 17:35:37 +0300

    shapes extracted from Piece

--- pieces.py
+++ pieces.py
@@ -13,42 +13,42 @@ class PieceType(Enum):
     S = auto()
     Z = auto()
 
-class Piece:    
-    SHAPES = {
-        PieceType.I: [
-        "    ",
-        "IIII",
-        "    ",
-        ],
-        PieceType.O: [
-        "OO",
-        "OO",
-        ],
-        PieceType.T: [
-        " T ",
-        "TTT",
-        ],
-        PieceType.J: [
-        "  J",
-        "JJJ",
-        ],
-        PieceType.L: [
-        "L  ",
-        "LLL",
-        ],
-        PieceType.S: [
-        "SS ",
-        " SS",
-        ],
-        PieceType.Z: [
-        " ZZ", 
-        "ZZ ",
-        ],
-    }
+SHAPES = {
+    PieceType.I: [
+    "    ",
+    "IIII",
+    "    ",
+    ],
+    PieceType.O: [
+    "OO",
+    "OO",
+    ],
+    PieceType.T: [
+    " T ",
+    "TTT",
+    ],
+    PieceType.J: [
+    "  J",
+    "JJJ",
+    ],
+    PieceType.L: [
+    "L  ",
+    "LLL",
+    ],
+    PieceType.S: [
+    "SS ",
+    " SS",
+    ],
+    PieceType.Z: [
+    " ZZ", 
+    "ZZ ",
+    ],
+}
 
+class Piece:
     def __init__(self, kind: PieceType, x: int = 0, y: int = 0):
         self.kind = kind
-        self.shape = [list(row) for row in self.SHAPES[kind]]
+        self.shape = [list(row) for row in SHAPES[kind]]
         self.x = x
         self.y = y
     
